522fa59b9f20000b7c357933b42fcebf6c2023a6
Alexander Busuttil Cazac <alexander.busuttil-cazac.24@um.edu.mt>
2024-10-23 19:24:36 +0200

    Minor change

--- Tutorials (Labs)/Lab 1/Lab1_Maths.c
+++ Tutorials (Labs)/Lab 1/Lab1_Maths.c
@@ -5,10 +5,10 @@ float mul(float num1, float num2);
 float div(float num1, float num2);
 
 int main (){
-	
+	z
 	float num1, num2;
 	
-	printf("Please enter number 1\nNumber 1: ");
+	printf("Please enter number 1\nNumber 1:  ");
 	scanf("%f", &num1);
 	printf("Please enter number 2\nNumber 2: ");
 	scanf("%f", &num2);
